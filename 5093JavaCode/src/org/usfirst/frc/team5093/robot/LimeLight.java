fdd695934ffe23782ffa929225b637b91e83f563
Adminsitrador <Adminsitrador@10.41.208.162>
2018-02-03 16:25:55 -0700

    Inicio del código de LimeLight Camera.

---
+++ 5093JavaCode/src/org/usfirst/frc/team5093/robot/LimeLight.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team5093.robot;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class LimeLight extends Command {
+	edu.wpi.first.networktables.NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+	NetworkTableEntry tx = table.getEntry("tx");
+	NetworkTableEntry ty = table.getEntry("ty");
+	NetworkTableEntry ta = table.getEntry("ta");
+	double x = tx.getDouble(0);
+	double y = ty.getDouble(0);
+	double area = ta.getDouble(0);
+	
+	
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
